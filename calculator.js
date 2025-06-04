3865f12d07914b9067a143ad839c7403128e0657
Anu <anupama12sg@gmail.com>
2025-06-04 15:57:36 +0530

    feat: arithmatic ops using switch stmt

--- calculator.js
+++ calculator.js
@@ -2,4 +2,26 @@ const prompt = require("prompt-sync")()
 
 const number1 = prompt("Enter number 1: ")
 const number2 = prompt("Enter number 2: ")
-const sign = prompt("Enter sign: ")
+const operator = prompt("Enter sign: ")
+
+let result;
+switch (operator) {
+    case "+":
+        result = number1 + number2
+        console.log(result)
+        break;
+    case "-":
+        result = number1 - number2
+        console.log(result)
+        break;
+    case "*":
+        result = number1 * number2
+        console.log(result)
+        break;
+    case "/":
+        result = number1 / number2
+        console.log(result)
+        break;
+
+}
+
