ee9d148d65e7d03ece778b4a1fc6360a2e497631
Anu <anupama12sg@gmail.com>
2025-06-04 16:59:10 +0530

    chore: added while loops for no. 1 & 2 to handle invalid input and NaN

--- calculator.js
+++ calculator.js
@@ -1,11 +1,31 @@
 const prompt = require("prompt-sync")()
 
-const number1 = parseFloat(prompt("Enter number 1: "))
-const number2 = parseFloat(prompt("Enter number 2: "))
+let number1;
+let number2;
+
+while (true) {
+    number1 = parseFloat(prompt("Enter number 1: "))
+    if (isNaN(number1)) {
+        console.log("Invalid input")
+    } else {
+        break;
+    }
+}
+
+
+while (true) {
+    number2 = parseFloat(prompt("Enter number 2: "))
+    if (isNaN(number2)) {
+        console.log("Invalid input")
+    } else {
+        break;
+    }
+}
+
 const operator = prompt("Enter sign: ")
 
 let result;
-let valid = true; 
+let valid = true;
 switch (operator) {
     case "+":
         result = number1 + number2     //8 + 9 = 89
@@ -24,6 +44,5 @@ switch (operator) {
         valid = false;
         break;
 }
-if (valid) 
+if (valid)
     console.log(number1, operator, number2, "=", result)
-
