99984e6c2915c33964983a09d566e29a3491bbce
Anu <anupama12sg@gmail.com>
2025-06-05 16:41:28 +0530

    chore: fucntion to display contact mgmt menu

---
+++ contact_mgmt.js
@@ -0,0 +1,13 @@
+const prompt =  require("prompt-sync")()
+
+function printInfo() {
+    console.log("Contact Management System")
+    console.log("-------------------------")
+    console.log("1. Add a contact")
+    console.log("2. Delete a contact")
+    console.log("3. View Contacts")
+    console.log("Search Contacts")
+    console.log("5. Exit")
+}
+
+printInfo()
