8ddb89161fb4755ce12a24f09d0d9d47b8b0b406
Anu <anupama12sg@gmail.com>
2025-06-08 13:58:39 +0530

    chore:Add input validation and move placement logic for 3x3 game board

--- tictactoe.js
+++ tictactoe.js
@@ -1,11 +1,19 @@
 const prompt = require("prompt-sync")()
 
 function makeMove(turn, board) {
-    const row = parseInt(prompt("Enter row: "))
-    const col = parseInt(prompt("Enter col: "))
+    while (true) {
+        const row = parseInt(prompt("Enter row: "))
+        const col = parseInt(prompt("Enter col: "))
 
-    if (isNaN(row) || row < 1 || row > 3) console.log("Invalid row.")
-    else if (isNaN(col) || col < 1 || col > 3) console.log("Invalid column.")
+        if (isNaN(row) || row < 1 || row > 3) console.log("Invalid row.")
+        else if (isNaN(col) || col < 1 || col > 3) console.log("Invalid column.")
+        else if (board[row - 1][col - 1] !== " ") console.log("Invalid position.")
+        else {
+            board[row - 1][col - 1] = turn
+            break
+        }
+    }
+
 }
 
 const board = [
