af8ad5c3dca9978267e587ea890b1f3cd443ae0e
Anu <anupama12sg@gmail.com>
2025-06-08 13:51:34 +0530

    chore: logic for invalid rows and cols

--- tictactoe.js
+++ tictactoe.js
@@ -3,8 +3,10 @@ const prompt = require("prompt-sync")()
 function makeMove(turn, board) {
     const row = parseInt(prompt("Enter row: "))
     const col = parseInt(prompt("Enter col: "))
-}
 
+    if (isNaN(row) || row < 1 || row > 3) console.log("Invalid row.")
+    else if (isNaN(col) || col < 1 || col > 3) console.log("Invalid column.")
+}
 
 const board = [
     [" ", " ", " "],
