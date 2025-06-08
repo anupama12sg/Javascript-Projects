dbeca04d5c13e018597d31cbe3a9bdc3784d1c25
Anu <anupama12sg@gmail.com>
2025-06-08 13:44:07 +0530

    chore: makeMove function

--- tictactoe.js
+++ tictactoe.js
@@ -1,8 +1,14 @@
-const board = [
-[" ", " ", " "],
-[" ", " ", " "],
-[" ", " ", " "]
+const prompt = require("prompt-sync")()
 
+function makeMove(turn, board) {
+    const row = prompt("Enter row: ")
+    const col = prompt("Enter col: ")
+}
+
+
+const board = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]
 ]
 
- 
